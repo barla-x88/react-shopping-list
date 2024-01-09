eab0916ede69221e8471fa7eba0afe0e473ce955
Barla Ankit <barla.x88@gmail.com>
2024-01-09 22:37:13 +0530

    Add functionality to save list in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,110 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import uniqid from 'uniqid';
 
 function App() {
-  const [list, setList] = useState([
-    { itemName: 'suger', qty: '5kg', bought: false, uid: crypto.randomUUID() },
-    { itemName: 'apples', qty: '1kg', bought: true, uid: crypto.randomUUID() },
-  ]);
+  const [list, setList] = useState([]);
+
+  const [itemName, setItemName] = useState('');
+  const [qty, setQty] = useState('');
+
+  const saveLocalList = () => {
+    localStorage.setItem('list', JSON.stringify(list));
+  };
+
+  const getLocalList = () => {
+    const localList = localStorage.getItem('list');
+    if (localList) {
+      setList(JSON.parse(localList));
+    }
+  };
+
+  //load local list on application start
+  useEffect(() => {
+    getLocalList();
+  }, []);
+
+  const clearList = () => {
+    setList([]);
+    localStorage.removeItem('list');
+  };
+
+  const addItem = () => {
+    setList([...list, { itemName, qty, bought: false, uid: uniqid() }]);
+    setItemName('');
+    setQty('');
+  };
 
   return (
     <main className="container h-100vh">
       {/* Heading */}
       <div className="row justify-content-center align-items-center my-4">
-        <div className="col">
-          <h1 className="text-center">shopping List</h1>
+        <div className="col bg-primary">
+          <h1 className="text-center text-white">shopping List</h1>
         </div>
       </div>
 
       {/* Buttons */}
       <div className="btn-row row justify-content-end mt-4">
         <div className="col-auto">
-          <button type="button" className="btn btn-primary text-nowrap btn-sm">
+          <button
+            onClick={saveLocalList}
+            type="button"
+            className="btn btn-primary text-nowrap "
+          >
             Save List
           </button>
         </div>
         <div className="col-auto">
-          <button type="button" className="btn btn-danger text-nowrap btn-sm">
-            Delete List
+          <button
+            onClick={clearList}
+            type="button"
+            className="btn btn-danger text-nowrap "
+          >
+            Clear List
           </button>
         </div>
       </div>
 
       {/* add new item */}
-      <div className="row mt-4 justify-content-center">
-        <div className="col-sm-12 col-md-auto ">
+      <div className="row mt-4 justify-content-center ">
+        <div className="col-sm-12 col-md-2 ">
           <label htmlFor="itemName" className="col-form-label">
             Item Name
           </label>
         </div>
-        <div className="col-sm-12 col-md-auto ">
-          <input type="text" id="itemName" className="form-control" />
+        <div className="col-sm-12 col-md-3 ">
+          <input
+            type="text"
+            id="itemName"
+            name="itemName"
+            value={itemName}
+            className="form-control"
+            onChange={(e) => {
+              setItemName(e.target.value);
+            }}
+          />
         </div>
-        <div className="col-sm-12 col-md-auto ">
+        <div className="col-sm-12 col-md-2 ">
           <label htmlFor="itemQuantity" className="col-form-label">
             Item Quantity
           </label>
         </div>
-        <div className="col-sm-12 col-md-auto mb-3">
-          <input type="text" id="itemQuantity" className="form-control" />
+        <div className="col-sm-12 col-md-3 mb-3">
+          <input
+            type="text"
+            name="qty"
+            id="itemQuantity"
+            value={qty}
+            className="form-control"
+            onChange={(e) => {
+              setQty(e.target.value);
+            }}
+          />
         </div>
-        <div className="col-sm-12 col-md-auto">
-          <button type="button" className="btn btn-success">
+        <div className="col-sm-12 col-md-2">
+          <button onClick={addItem} type="button" className="btn btn-success">
             Add Item
           </button>
         </div>
@@ -71,6 +125,16 @@ function App() {
                       type="checkbox"
                       className="form-check-input me-1"
                       checked={item.bought}
+                      onChange={(e) => {
+                        // Modify current item
+                        const currentItem = list.find(
+                          (current) => current.uid === item.uid
+                        );
+                        currentItem.bought = e.target.checked;
+
+                        // re-render list
+                        setList([...list]);
+                      }}
                     />
                     {item.itemName}
                   </span>
